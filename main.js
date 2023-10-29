a1386cb65300446be0c876e9cf21f38bd51bb7a4
gilesb <giles.bradshaw@sigyl.com>
2023-10-29 19:49:01 +0000

    ....

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const addRelease = (
 ) => `${component.replace(/\d/g,'')}${Number(component.replace(/\D/g,'') || 0) + amount}`
 
 const versionUp = (current, level, amount) => {
-  // from https://raw.githubusercontent.com/okunishinishi/node-versionup/master/lib/_next_version.js
-  // get id of prerelease
   const [value] = String(current).split('-')
   const components = value.split(/\./)
   const major = components[0] || 0;
